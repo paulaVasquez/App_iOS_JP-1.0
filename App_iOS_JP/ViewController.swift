19ed190e4782e4d1781a7893021e7c59f3b34e9e
paulavaz <paulavaz@unicauca.edu.co>
2019-07-13 14:11:39 -0500

    Initial Commit

---
+++ App_iOS_JP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App_iOS_JP
+//
+//  Created by Telematica on 7/12/19.
+//  Copyright © 2019 ApliMovil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
